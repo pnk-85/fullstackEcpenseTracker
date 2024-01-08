5178073638a8d45e9149efa5f941d789ca16e0cb
pnk-85 <pnkghogare27@gmail.com>
2024-01-08 14:06:13 +0530

    creating signup screen backend

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const sequelize = require('./util/database');
+const Expense = require('./models/Expense');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json({extended:false}));
+
+const expenseRoutes = require('./routes/expense');
+
+app.use('/expense',expenseRoutes);
+
+sequelize.sync().then((response) =>{
+    console.log(response);
+    app.listen(3000);
+}).catch(error=>console.log(error));
---
+++ util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node_complete','root','pankaj@17',{
+    dialect: 'mysql',
+    host: 'localhost'
+});
+
+module.exports = sequelize;
